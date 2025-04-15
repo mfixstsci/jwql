213804bf26d9b11c7ec82900a3c1963bce5bbae9
york@stsci.edu <york@stsci.edu>
2025-04-15 15:37:15 -0400

    Another typo? How surprising

--- jwql/website/apps/jwql/views.py
+++ jwql/website/apps/jwql/views.py
@@ -1264,7 +1264,7 @@ def view_exposure(request, inst, group_root):
     return render(request, template, context)
 
 
-def view_image(request, inst, file_root):
+def view_image(request, inst, file_root, suffix=""):
     """Generate the image view page
 
     Parameters
@@ -1283,7 +1283,6 @@ def view_image(request, inst, file_root):
     HttpResponse object
         Outgoing response sent to the webpage
     """
-    suffix = request.POST.get("suffix")
     log_file = configure_logging("django", include_time=False)
     logging.info(f"Running through view_image() for {inst} {file_root} suffix={suffix}")
 

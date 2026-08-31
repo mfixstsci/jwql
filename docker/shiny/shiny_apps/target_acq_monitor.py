d714614bdbecd05556314e94ce3d52e29597fb59
york@stsci.edu <york@stsci.edu>
2026-08-31 15:11:19 -0400

    And adding an environment check

--- docker/shiny/shiny_apps/target_acq_monitor.py
+++ docker/shiny/shiny_apps/target_acq_monitor.py
@@ -12,6 +12,7 @@ import numpy as np
 import os
 
 running_standalone = str(os.environ.get("SHINY_EMBED", 0)) != "0"
+logging.info(f"SHINY_EMBED={os.environ.get('SHINY_EMBED')}")
 logging.info(f"Running Standalone: {running_standalone}")
 
 plt.rcParams["font.weight"] = "bold"
